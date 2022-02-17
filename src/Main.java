090b6326d8eb21a378846d28e937e1098ef3920a
kaitlyn <knie8242@bths.edu>
2022-02-16 21:59:52 -0500

    Updating code: 2/16/22

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        // creating ArrayLists for the files
         ArrayList<String> gifs = new ArrayList<String>();
         ArrayList<String> pictures = new ArrayList<String>();
         ArrayList<String> messages = new ArrayList<String>();
         ArrayList<String> reminders = new ArrayList<String>();
 
+
+        // putting all the files into an ArrayList
+        // gifs
         try {
             File f = new File ("Animal Gifs");
             BufferedReader br = new BufferedReader(new FileReader(f));
@@ -21,12 +25,35 @@ public class Main {
         }
         catch (Exception e) { }
 
+        // pictures
         try {
             File f = new File ("Animal Pictures");
             BufferedReader br = new BufferedReader(new FileReader(f));
             String s = "";
             while ((s = br.readLine()) != null) {
                 pictures.add(s);
+            }
+        }
+        catch (Exception e) { }
+
+        // messages
+        try {
+            File f = new File ("Messages");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String s = "";
+            while ((s = br.readLine()) != null) {
+                messages.add(s);
+            }
+        }
+        catch (Exception e) { }
+
+        // reminders
+        try {
+            File f = new File ("Reminders");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String s = "";
+            while ((s = br.readLine()) != null) {
+                reminders.add(s);
             }
         }
         catch (Exception e) { }
