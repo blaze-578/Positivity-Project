8b0c6188f23388fdefaab63060a2f1e3439e4e40
Nicole Huang <nicoleh87@nycstudents.net>
2022-05-16 11:38:02 -0400

    5/16/22 IN SCHOOL

--- src/MainActionCommands.java
+++ src/MainActionCommands.java
@@ -1,3 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainActionCommands {
     private String name;
     private Color color;
@@ -10,6 +16,37 @@ public class MainActionCommands {
         color = c;
         startingNumber = s;
 
+    }
+
+    public void saveUserInfo() {
+        try {
+            File f = new File("src/user.data");
+            f.createNewFile(); // this method will create the file if it does not exist, if it does exist, it does nothing
+            FileWriter fw = new FileWriter("src/user.data");
+            fw.write(name + "\n" + color + "\n" + startingNumber);
+            fw.close();
+        }
+        catch (IOException e) {
+            System.out.println("Unable to create file");
+            e.printStackTrace();
+        }
+    }
+
+    public void saveInfo(ArrayList<String> list, String fileName) {
+        try {
+            File f = new File(fileName);
+            f.createNewFile(); // this method will create the file if it does not exist, if it does exist, it does nothing
+            FileWriter fw = new FileWriter(fileName);
+            for (int i = 0; i < list.size() - 1; i++) {
+                fw.write(list.get(i) + "\n");
+            }
+            fw.write(list.get(list.size() - 1));
+            fw.close();
+        }
+        catch (IOException e) {
+            System.out.println("Unable to create file");
+            e.printStackTrace();
+        }
     }
 
 
