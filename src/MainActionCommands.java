97261f6d7b0a393aee140aca4b5738b56b021b70
Nicole Huang <nicoleh87@nycstudents.net>
2022-05-16 11:54:16 -0400

    5/16/22 IN SCHOOL

--- src/MainActionCommands.java
+++ src/MainActionCommands.java
@@ -5,32 +5,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActionCommands {
-    private String name;
-    private Color color;
-    private int startingNumber;
-    private int menu;
-
-    MainActionCommands(String n, Color c, int s) {
-        // main constructor
-        name = n;
-        color = c;
-        startingNumber = s;
-
-    }
-
-    public void saveUserInfo() {
-        try {
-            File f = new File("src/user.data");
-            f.createNewFile(); // this method will create the file if it does not exist, if it does exist, it does nothing
-            FileWriter fw = new FileWriter("src/user.data");
-            fw.write(name + "\n" + color + "\n" + startingNumber);
-            fw.close();
-        }
-        catch (IOException e) {
-            System.out.println("Unable to create file");
-            e.printStackTrace();
-        }
-    }
 
     public void saveInfo(ArrayList<String> list, String fileName) {
         try {
--- src/User.java
+++ src/User.java
@@ -1,3 +1,31 @@
-public class User {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class User {
+    private String name;
+    private Color color;
+    private int startingNumber;
+    private int menu;
+
+    public User(String n, Color c, int s) {
+        // main constructor
+        name = n;
+        color = c;
+        startingNumber = s;
+    }
+
+    public void saveUserInfo() {
+        try {
+            File f = new File("src/user.data");
+            f.createNewFile(); // this method will create the file if it does not exist, if it does exist, it does nothing
+            FileWriter fw = new FileWriter("src/user.data");
+            fw.write(name + "\n" + color + "\n" + startingNumber);
+            fw.close();
+        }
+        catch (IOException e) {
+            System.out.println("Unable to create file");
+            e.printStackTrace();
+        }
+    }
 }
