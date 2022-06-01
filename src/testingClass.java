a5c9b9db5f3b182cbba114f648e03ceb0cccff71
kaitlyn <knie8242@bths.edu>
2022-05-31 22:24:54 -0400

    5/31/22 AT HOME

--- src/testingClass.java
+++ src/testingClass.java
@@ -5,17 +5,9 @@ public class testingClass {
     public static void main(String[] args) throws InterruptedException {
         MainCmds m = new MainCmds("src/AllFiles.txt");
 
-        //ArrayList<String> temp = m.getGifs();
-        /*for (String i : temp) {
-            System.out.println(i);
-        }*/
-        //m.updateFiles("src/AnimalGifs.txt", m.getGifs());
-
-        //System.out.println(m.getMessages());
-
         String name = User.welcomeMessage();
 
-        User n = new User();
+        User n = new User(name);
         m.menu();
     }
 }
