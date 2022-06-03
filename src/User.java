291432b329ed18063c196d3175871c9328446927
Nicole Huang <nicoleh87@nycstudents.net>
2022-06-03 12:12:22 -0400

    6/3/22 IN SCHOOL

--- src/User.java
+++ src/User.java
@@ -10,7 +10,6 @@ public class User {
     private MainCmds m;
 
     public User() throws InterruptedException  {
-        name = "";
         m = new MainCmds();
         welcomeMessage();
     }
@@ -19,10 +18,12 @@ public class User {
         try {
             File f = new File("src/PersonData.txt");
             Scanner s = new Scanner(f);
-            String name = s.nextLine();
+            while (s.hasNextLine()) {
+                name = s.nextLine();
+            }
             m.createLists("src/RemainingMessages.txt");
 
-            System.out.println("\nWelcome back to the " + Color.BLUE_BRIGHT + "Positivity Machine " + Color.WHITE_BRIGHT + name + ", I hope your day is going well.");
+            System.out.println(Color.BLUE_BRIGHT + "\nWelcome back to the " + Color.BLUE_BRIGHT + "Positivity Machine " + Color.WHITE_BRIGHT + name + ", I hope your day is going well.");
             Thread.sleep(2000);
             System.out.println("If not, I hope this helps!\n");
             Thread.sleep(3500);
